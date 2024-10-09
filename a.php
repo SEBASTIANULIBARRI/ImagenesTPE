bc68c77ad68f15bb03ef22982234ed57c47eab4c
SEBASTIANULIBARRI <sebastiannahuelulibarri@gmail.com>
2024-10-09 18:54:23 -0300

    agregado de css

--- a.php
+++ a.php
@@ -8,4 +8,17 @@ echo "$ruta";
 
 ?>
 
-<img src="<?= $ruta?>" alt="">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <img src="<?= $ruta?>" alt="">  
+</body>
+</html>
+
+
